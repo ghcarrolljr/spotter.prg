30ec824649de9651b74649663349169b465738d6
George Carroll <ghcarrolljr@gmail.com>
2019-11-26 16:08:52 -0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+	// IP address or host name to the server hosting the database
+	$host_name = "db5000222557.hosting-data.io";
+
+	// Database username
+	$user_name = "dbu341512";
+	
+	// Database password
+	$password = "1234Passw0rd?";
+	
+	// Database schema name
+	$database = "dbs217277";
+
+?>
