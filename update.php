1f67810287a744ad9440485f3c34ce173c869f08
George Carroll <ghcarrolljr@gmail.com>
2020-10-22 16:26:17 -0500

    Update update.php

--- update.php
+++ update.php
@@ -8,8 +8,10 @@
 				document.getElementById("url").value = "https://www.maxpreps.com/high-schools/pulaski-academy-bruins-(little-rock,ar)/football/roster.htm";
 			}
             function radOpp_clicked() {
-				document.getElementById("url").value = "https://www.maxpreps.com/high-schools/pulaski-academy-bruins-(little-rock,ar)/football/roster.htm";
+                var school = document.getElementById("school").value;
+				document.getElementById("url").value = "https://www.maxpreps.com/high-schools/" + school + "/football/roster.htm";
 			}
+            
 		</script>
         <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
         <meta content="utf-8" http-equiv="encoding">
@@ -19,88 +21,95 @@
     <body>
     	<form name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 			Update which team:
-			<label for="radBruins">Bruins</label><input type="radio" name="team" id="radBruins" value="BRoster" onclick="radBruins_clicked()"> 
-			<label for="radOpp">Opponent</label><input type="radio" name="team" id="radOpp" value="ORoster" onclick="radOpp_clicked()"><br>
-			<label for="url">Maxpreps roster URL: </label><input type="text" name="url" id="url" style="width: 200px;" /><br>
+            
+            
+			<label for="radBruins">Bruins</label><input type="radio" name="team" id="radBruins" value="BRoster" onclick="radBruins_clicked()">
+			<br>
+            <select id="school">
+                <option value = "little-rock-christian-academy-warriors-(little-rock,ar)">Little Rock Christian Academy</option>
+                <option value="harrison-goblins-(harrison,ar)">Harrison</option>
+                <option value="valley-view-blazers-(jonesboro,ar)">Valley View</option>
+                <option value="maumelle-hornets-(maumelle,ar)">Maumelle</option>
+                <option value="lakeside-rams-(hot-springs,ar)">Lakeside</option>
+                <option value="forrest-city-mustangs-(forrest-city,ar)">Forrest City</option>
+                <option value="arkansas-razorbacks-(texarkana,ar)">Arkansas</option>
+                <option value="vilonia-eagles-(vilonia,ar)">Vilonia</option>
+                <option value="magnolia-panthers-(magnolia,ar)">Magnolia</option>
+                <option value="white-hall-bulldogs-(white-hall,ar)">White Hall</option>
+                <option value="morrilton-devil-dogs-(morrilton,ar)">Morrilton</option>
+                <option value="farmington-cardinals-(farmington,ar)">Farmington</option>
+                <option value="high-schools/nettleton-raiders-(jonesboro,ar)">Nettleton</option>
+                <option value="wynne-yellowjackets-(wynne,ar)">Wynne</option>
+                <option value="camden-fairview-cardinals-(camden,ar)">Camden Fairview</option>
+                <option value="hot-springs-trojans-(hot-springs,ar)">Hot Springs</option>
+                <option value="greenbrier-panthers-(greenbrier,ar)">Greenbrier</option>
+                <option value="watson-chapel-wildcats-(pine-bluff,ar)">Watson Chapel</option>
+                <option value="alma-airedales-(alma,ar)">Alma</option>
+                <option value="mcclellan-crimson-lions-(little-rock,ar)">McClellan</option>
+                <option value="clarksville-panthers-(clarksville,ar)">Clarksville</option>
+                <option value="parkview-patriots-(little-rock,ar)">Parkview</option>
+                <option value="batesville-pioneers-(batesville,ar)">Batesville</option>
+                <option value="blytheville-chickasaws-(blytheville,ar)">Blytheville</option>
+                <option value="greene-county-tech-golden-eagles-(paragould,ar)">Greene County Tech</option>
+                <option value="/beebe-badgers-(beebe,ar)">Beebe</option>
+                <option value="/huntsville-eagles-(huntsville,ar)">Huntsville</option>
+                <option value="hope-bobcats-(hope,ar)">Hope</option>
+                <option value="paragould-rams-(paragould,ar)">Paragould</option>
+                <option value="fair-war-eagles-(little-rock,ar)">Fair</option>
+                <option value="de-queen-leopards-(de-queen,ar)">De Queen</option>
+                <option value="robinson-senators-(little-rock,ar)">Robinson</option>
+            </select>
+            <label for="radOpp">Opponent</label><input type="radio" name="team" id="radOpp" value="ORoster" onclick="radOpp_clicked()"><br> 
+			<label for="url">Maxpreps roster URL: </label><input type="text" name="url" id="url" style="width: 600px;" /><br>
 			<input type="submit" value="Submit" />
         </form>
-        <div id="topBar">
-            <a href ="#" id="load_home"> HOME </a>
-        </div>
-        <div id ="content">        
-        </div>
 
-        <script>
-        $(document).ready( function() {
-            $("#load_home").on("click", function() {
-                $("#content").load("hhttps://www.maxpreps.com/high-schools/pulaski-academy-bruins-(little-rock,ar)/football/roster.htm");
-            });
-        });
-            var s = document.getElementById("content").innerHTML;
-
-            var obj = JSON.parse(s);
-            document.getElementById("url").value = obj.lastName;
-            
-        </script>
+        
+        
 		<p>
             <?php 
-            
-            
-            /*
             include '../.sec/config.php';
+            $output = "";
             if (!empty($_POST["url"])) {
-
-                $table = $_POST["team"];
-                
-                $url = 'https://www.maxpreps.com/high-schools/pulaski-academy-bruins-(little-rock,ar)/football/roster.htm';
- 
-                //read json file from url in php
-                $readJSONFile = file_get_contents($url);
-                //convert json to array in php
-                $array = json_decode($readJSONFile);
-                var_dump($array); // print array
-
-
-                
- 
-                
-
-                
-                
-                
-                
-                
-                $url = "https://wt-90a32ef8ce619cb1158f5b6850fb1f12-0.sandbox.auth0-extend.com/maxprepsTeamScraper?url=" . $_POST["url"]; // path to the JSON file
-                $data = fetch($_POST["url"]);  // load contents of the page 
-                $roster = json_decode($data, true); // parse the JSON into an object "firstName":"Josiah","lastName":"Johnson","classYear":12,"jersey":"23","heightInches":10,"heightFeet":5,"weight":195,"position1":"SS","position2":"RB","position3":null,"hasStats":null,"isCaptain":false,"isDeleted":false,"photoUrl":null
-                echo $roster;*/
-                /*$output = "INSERT INTO $table (Number, Name, Class, Position) VALUES ";
-                if (($handle = fopen($table . ".csv", "r")) !== FALSE) {
-                       while (($roster = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                        //foreach ($roster as $player) {
-                                if ($roster[0]){
-                                    $output = $output . "('" . $roster[0] . "','" . $roster[1] . "','" . $roster[2] . "','" . $roster[3] . "'), ";
-                                    }
-
-                            //}
-                       }
+            $table = $_POST["team"];
+            $url = $_POST["url"];
+            //read json file from url in php
+            $readJSONFile = file_get_contents($url, true);
+            $num1 = strpos($readJSONFile, '<script id="__NEXT_DATA__" type="application/json">') + 51;
+            $json = substr($readJSONFile, $num1);
+            $num2 = strpos($json, '</script>');
+            $json = substr($readJSONFile, $num1, $num2);
+            $array = json_decode($json,true);// [roster], firstName, lastName, jersey, classYear, position1, position2, position3
+            //echo "<pre>";
+            $roster = $array[props][pageProps][roster];
+            //print_r($roster);
+            $i=0;
+            $output = "INSERT INTO $table (Number, Name, Class, Position) VALUES ";
+            foreach ($roster as $player) {
+                if ($roster[$i][jersey]) {
+                    $nameHold = Preg_replace("/[^a-zA-Z0-9_ -]/s"," ",$roster[$i][formattedName]);
+                    $output = $output . " ('" . $roster[$i][jersey] . "','" . $nameHold . "','" . $roster[$i][formattedClassYear] . "','" . $roster[$i][formattedPositions] . "'), ";
                     }
-                $output = $output . "(null,null,null,null);";
-                
-                $conn = mysqli_connect($host_name, $user_name, $password, $database);
-                if (mysqli_connect_errno()) {
-                  die('<p>Failed to connect to MySQL: '.mysqli_connect_error().'</p>');
-                } 
+                $i=$i+1;
+                }
+
+            $output = $output . "(null,null,null,null);";
+            $conn = mysqli_connect($host_name, $user_name, $password, $database);
+            if (mysqli_connect_errno()) {
+                    die('<p>Failed to connect to MySQL: '.mysqli_connect_error().'</p>');
+                    } 
                 else
-                {mysqli_query($conn, "DELETE FROM $table;");
-                if (mysqli_query($conn, $output) == false) {echo "error" . (mysqli_error($conn));}
-                 // Execute the insert statement
-                mysqli_close($conn);
-                 echo "Completed Successfully";
-                } 
+                    {mysqli_query($conn, "DELETE FROM $table;");
+                    if (mysqli_query($conn, $output) == false) {
+                        echo "-ERROR- " . (mysqli_error($conn));}
+                        // Execute the insert statement
+                     else {
+                        mysqli_close($conn);
+                        echo "Completed Successfully";}
+                    } 
             }
-            */
             ?>
         </p>
 	</body>
 </html>
+
